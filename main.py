c8ca7eac3d8d612fbe7a16444f22067ad4cff210
Elena <ealvar03@mail.bbk.ac.uk>
2023-02-16 17:22:38 +0000

    Upgraded blog version 1.03

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 
 blog_url = "https://api.npoint.io/06e77261250810e905f8"
 response = requests.get(blog_url)
 posts = response.json()
+my_email = "mail"
+password = "your_password"
 
 app = Flask(__name__)
 
@@ -25,12 +28,18 @@ def get_about():
     return render_template('about.html')
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def get_contact():
     """
-    When clicking on the "contact" section, the user will receive the "contact.html" file and its information
+    When clicking on the "contact" section, the user will receive the "contact.html" file and its information, when
+    filling the form, it will collect the data, and afterwards it will show a "successful sent" message on the screen
     """
-    return render_template('contact.html')
+    if request.method == "POST":
+        data = request.form
+        send_mail(data["username"], data["email"], data["phone"], data["message"])
+        return render_template('contact.html', sent=True)
+    else:
+        return render_template('contact.html', sent=False)
 
 
 @app.route("/posts/<int:blog_id>")
@@ -43,6 +52,15 @@ def get_blog(blog_id):
         if post_entry["id"] == blog_id:
             requested_blog = post_entry
     return render_template("post.html", post=requested_blog)
+
+
+def send_mail(name, email, phone, message):
+    user_message = f"Subject: New message\n\nName: {name}\nEmail: {email}\n{phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=my_email,
+                            msg=user_message)
 
 
 if __name__ == "__main__":
